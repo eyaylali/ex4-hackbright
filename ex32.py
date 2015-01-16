6ce7e36cd9e960fb98c4a1a8e2c1e78f467ad869
Hackbright Student <info@hackbrightacademy.com>
2015-01-15 16:26:27 -0800

    finished ex32

--- ex32.py
+++ ex32.py
@@ -1,30 +1,48 @@
-the_count = [1, 2, 3, 4, 5]
-fruits = ['apples', 'oranges', 'pears', 'apricots']
-change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+# the_count = [1, 2, 3, 4, 5]
+# fruits = ['apples', 'oranges', 'pears', 'apricots']
+# change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
 
-# this first kind of for-loop goes through a list
-for number in the_count:
-    print "This is count %d" % number
+# # this first kind of for-loop goes through a list
+# for number in the_count:
+#     print "This is count %d" % number
 
-# same as above
-for fruit in fruits:
-    print "A fruit of type: %s" % fruit
+# # same as above
+# for fruit in fruits:
+#     print "A fruit of type: %s" % fruit
 
-# also we can go through mixed lists too
-# notice we have to use %r since we don't know what's in it
-for i in change:
-    print "I got %r" % i
+# # also we can go through mixed lists too
+# # notice we have to use %r since we don't know what's in it
+# for i in change:
+#     print "I got %r" % i
 
-# we can also build lists, first start with an empty one
-elements = []
+# # we can also build lists, first start with an empty one
+# elements = []
 
-# then use the range function to do 0 to 5 counts
-for i in range(0, 6):
-    print "Adding %d to the list." % i
-    # append is a function that lists understand
-    elements.append(i)
+# # then use the range function to do 0 to 5 counts
+# for i in range(0, 6):
+#     print "Adding %d to the list." % i
+#     # append is a function that lists understand
+#     elements.append(i)
 
-# now we can print them out too
-for i in elements:
-    print "Element was: %d" % i
+# # now we can print them out too
+# for i in elements:
+    # print "Element was: %d" % i
+
+new_list = []
+new_list2 = []
+final_list = []
+
+
+for i in range(1,4):
+    new_list.append(i)
+for i in range(4,7):
+    new_list2.append(i)
+
+final_list.extend(new_list)
+final_list.extend([new_list])
+final_list.extend([new_list, new_list2, "hi"])
+
+final_list.append("end")
+
+print final_list
 
